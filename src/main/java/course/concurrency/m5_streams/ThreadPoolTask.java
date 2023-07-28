c145b9cd991cd1729a37232667f002c5793df474
alejandro <zavoikin.a@olimp.dev>
2023-07-28 12:12:24 +0300

    Added executors

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -6,11 +6,19 @@ public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        final BlockingQueue<Runnable> queue = new LinkedBlockingDeque<>() {
+            @Override
+            public boolean offer(Runnable runnable) {
+                return super.offerFirst(runnable);
+            }
+        };
+        return new ThreadPoolExecutor(1, 1, 60, TimeUnit.SECONDS, queue);
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        final BlockingQueue<Runnable> queue = new SynchronousQueue<>();
+        final RejectedExecutionHandler handler = new ThreadPoolExecutor.DiscardPolicy();
+        return new ThreadPoolExecutor(8, 8, 60, TimeUnit.SECONDS, queue, handler);
     }
 }
